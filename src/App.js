59cd4d919b4c4517ca3ab2f253f31dec473663a4
Hariz <syedhariz1995@gmail.com>
2023-03-01 21:26:56 +0800

    Added checkout number on cart logo in te Nav

--- src/App.js
+++ src/App.js
@@ -24,6 +24,14 @@ function App() {
     setCart(cart.filter(book => book.id !== item.id ))
   }
 
+  function numberOfItems(){
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity
+    })
+    return counter
+  }
+
   useEffect(() => {
     console.log(cart)
   }, [cart])
@@ -31,7 +39,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-      <Nav/>
+      <Nav numberOfItems={numberOfItems()}/>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/books" element={<Books books={books}/>}/> 
